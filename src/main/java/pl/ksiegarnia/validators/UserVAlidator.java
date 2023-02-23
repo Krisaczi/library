3638e2c589ffe95322c00be89b0e8ed85f0aac7f
Krzysztof <krisaczi@yahoo.com>
2023-02-23 18:51:47 +0100

    javascript validacja

--- src/main/java/pl/ksiegarnia/validators/UserVAlidator.java
+++ src/main/java/pl/ksiegarnia/validators/UserVAlidator.java
@@ -1,16 +1,25 @@
 package pl.ksiegarnia.validators;
 
+import com.fasterxml.jackson.databind.deser.DataFormatReaders;
 import pl.ksiegarnia.exceptions.UserValidationException;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserVAlidator {
+
+
     public static void validateLogin(String login){
-        if(login.contains(" ") || login.length() < 5){
+        String regex = "^[a-zA-Z0-9]{5,}$";
+
+        if(!login.matches(regex)){
             throw new UserValidationException();
         }
     }
 
     public static void validatePassword(String password){
-        if(password.contains(" ") || password.length()<5){
+        String regex = "^[a-zA-Z0-9]{5,}$";
+        if(!password.matches(regex)){
             throw new UserValidationException();
         }
     }
